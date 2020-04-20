a7b67c5eab69fbef41535aa5bd0a023f11c27e62
sekiya9311 <sekiya9311@gmail.com>
2020-04-21 00:24:59 +0900

    Add func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((_, response) => {
   response.send("Hello from Firebase!");
+});
+
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const addCO2 = functions.https.onCall(async (data, _) => {
+  const co2 = data.co2 as Number;
+  const nowTimestamp = admin.firestore
+    .Timestamp.fromMillis(Date.now());
+  const addObj = {
+    co2,
+    created_at: nowTimestamp
+  };
+
+  await firestore
+    .collection('measure_items')
+    .add(addObj);
 });
